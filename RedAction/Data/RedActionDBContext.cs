430914e82dbd209a83a318b5082f0b9efc2db193
angel <angel@LAPTOP-D68A60BE>
2023-06-25 17:48:00 -0300

    Agregar archivos de proyecto.

---
+++ RedAction/Data/RedActionDBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RedAction.Models;
+
+    public class RedActionDBContext : DbContext
+    {
+        public RedActionDBContext (DbContextOptions<RedActionDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RedAction.Models.Articulo> Articulo { get; set; } = default!;
+
+        public DbSet<RedAction.Models.Usuario>? Usuario { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=LAPTOP-D68A60BE\SQLEXPRESS;Database=RedActionDBMVC-G;Trusted_Connection=True;");
+    }
+}
